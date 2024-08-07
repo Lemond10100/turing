b19a5d4a40fe82ad1e5c9b6517943b5942652b89
Domenico Tuttolomondo <dtuttolomondo.idm@gmail.com>
2024-08-07 19:28:15 +0200

    push5, solo la modifica perfchè sfuggita. ancora non funzionante

---
+++ src/main/java/servletsControllers/ModificaPersonaServlet.java
@@ -0,0 +1,28 @@
+package servletsControllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Persona;
+import services.GestioneRubricaService;
+import java.io.IOException;
+
+public class ModificaPersonaServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        String nome = request.getParameter("nome");
+        String cognome = request.getParameter("cognome");
+        String indirizzo = request.getParameter("indirizzo");
+        String telefono = request.getParameter("telefono");
+        int eta = Integer.parseInt(request.getParameter("eta"));
+
+        Persona persona = new Persona(id, nome, cognome, indirizzo, telefono, eta);
+
+        GestioneRubricaService service = new GestioneRubricaService();
+        service.modificaPersona(persona);
+
+        response.sendRedirect("lista.jsp");
+    }
+}
