9b4d5926b8d0cc6c8b9b20695aa51d0c7da5ddb9
Domenico Tuttolomondo <dtuttolomondo.idm@gmail.com>
2024-08-12 14:37:05 +0200

    push17,last fixes

--- src/main/resources/schema_database.sql
+++ src/main/resources/schema_database.sql
@@ -9,3 +9,30 @@ CREATE TABLE lista_contatti (
     telefono VARCHAR(255),
     eta INT
 );
+
+
+
+INSERT INTO lista_contatti (nome, cognome, indirizzo, telefono, eta, username)
+VALUES ('Mario', 'Rossi', 'Via Roma 12, Roma', '+391234567890', 35, 'mario.rossi');
+
+
+INSERT INTO lista_contatti (nome, cognome, indirizzo, telefono, eta, username)
+VALUES ('Luca', 'Bianchi', 'Corso Venezia 11, Milano', '+391234567891', 40, 'lucaB88');
+
+
+INSERT INTO lista_contatti (nome, cognome, indirizzo, telefono, eta, username)
+VALUES ('Giulia', 'Verdi', 'Piazza del Popolo 5, Napoli', '+391234567892', 28, 'giuliaV23');
+
+
+INSERT INTO lista_contatti (nome, cognome, indirizzo, telefono, eta, username)
+VALUES ('Sofia', 'Neri', 'Via Larga 22, Firenze', '+391234567893', 30, 'sofia_neri');
+
+
+
+
+
+CREATE TABLE users (
+    username VARCHAR(30) PRIMARY KEY,
+    password VARCHAR(255) NOT NULL
+);
+
